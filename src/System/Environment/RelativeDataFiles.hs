4352b21245f04bb06544f17378fab3ab677d300b
Peter Trško <peter.trsko@gmail.com>
2015-01-24 16:21:05 +0100

    Users may adjust variable names and directory layout
    This was done by extracting all environment variable names and path
    modification functions in to a data type named Parameters. It provides
    default value using Default instance. Most of the default values were
    inspired by Cabal's autogenerated Paths_* modules.

--- src/System/Environment/RelativeDataFiles.hs
+++ src/System/Environment/RelativeDataFiles.hs
@@ -12,14 +12,20 @@
 -- TODO
 module System.Environment.RelativeDataFiles
     (
-    -- $intro
-      getBaseDir
+    -- * Environment Variables and Directory Layout
+    -- $directoryLayout
+      module System.Environment.RelativeDataFiles.Parameters
+    , module Data.Default.Class
+
+    -- * Get Directories File Path
+    , getBaseDir
     , getBinDir
     , getDataDir
     , getSysconfigDir
     , getLibDir
     , getLibexecDir
 
+    -- * Get Data File Path
     , getDataFileName
     )
   where
@@ -32,37 +38,47 @@ import Data.Maybe (Maybe(Just, Nothing))
 import Data.Proxy (Proxy(Proxy))
 import Data.String (String)
 import Data.Tuple (fst)
-import System.Environment (getEnv)
+import qualified System.Environment as Environment (getEnv)
 import System.IO (IO, FilePath)
 
-import System.FilePath ((</>), dropFileName, dropTrailingPathSeparator)
+import System.FilePath ((</>))
 
+import Data.Default.Class
 import System.Environment.Executable (splitExecutablePath)
+
+import System.Environment.RelativeDataFiles.Parameters
 
 
 -- | Return \"bin\" directory.
 --
 -- Algorithm is as follows:
 --
--- 1. Check if environment variable @data_files_bindir@ is defined. If it is,
---    then return its value.
+-- 1. Check if environment variable for bin directory is defined. If it is,
+--    then return its value. See 'binDirEnvVar' for additional information.
 --
--- 2. Check if environment variable @data_files_basedir@ is defined. If it is,
---    then return its value with @('</>' \"bin\")@ appended to it.
+-- 2. Check if environment variable base dir is defined. If it is,
+--    then return its value with modified using 'baseDirToBinDir'. See
+--    'baseDirEnvVar' and 'baseDirToBinDir' for additional information.
 --
 -- 3. Get file path of currently executed program and return directory where it
---    resides.
-getBinDir :: IO FilePath
-getBinDir = do
-    maybeBinDir <- getEnv' "data_files_bindir"
+--    resides and apply 'executableDirToBinDir' to it. See
+--    'executableDirToBinDir' for additional details.
+getBinDir :: Parameters -> IO FilePath
+getBinDir params = do
+    maybeBinDir <- getEnv binDirVariableName
     case maybeBinDir of
         Just binDir -> return binDir
         Nothing     -> do
-            maybeBaseDir <- getEnv' "data_files_basedir"
+            maybeBaseDir <- getEnv baseDirVariableName
             case maybeBaseDir of
-                Just baseDir -> return $ baseDir </> "bin"
+                Just baseDir -> return $ modifyBaseDir baseDir
                 Nothing      ->
-                    dropTrailingPathSeparator . fst <$> splitExecutablePath
+                    modifyExecutableDir . fst <$> splitExecutablePath
+  where
+    baseDirVariableName = baseDirEnvVar params
+    binDirVariableName  = binDirEnvVar params
+    modifyBaseDir       = baseDirToBinDir params
+    modifyExecutableDir = executableDirToBinDir params
 
 -- | Return base direcotory where application is installed.
 --
@@ -72,50 +88,60 @@ getBinDir = do
 --    then return its value.
 --
 -- 2. Call 'getBaseDir' and drop last directory element (\"bin\").
-getBaseDir :: IO FilePath
-getBaseDir = do
-    maybeBaseDir <- getEnv' "data_files_basedir"
+getBaseDir :: Parameters -> IO FilePath
+getBaseDir params = do
+    maybeBaseDir <- getEnv baseDirVariableName
     case maybeBaseDir of
         Just baseDir -> return baseDir
-        Nothing      -> dropTrailingPathSeparator . dropFileName <$> getBinDir
+        Nothing      -> binDirToBaseDir <$> getBinDir params
+  where
+    baseDirVariableName = baseDirEnvVar params
+    binDirToBaseDir     = executableDirToBaseDir params
 
 -- | Utility function that is used for implementing a lot of other functions
 -- in this module. It is not exported.
 stdGetDir
-    :: String
-    -- ^ Environment variable name to check for its value.
-    -> FilePath
-    -- ^ Subdirectory of base directory. See 'getBaseDir' for details.
+    :: (Parameters -> String)
+    -- ^ Getter for environment variable name that is check for its value.
+    -> (Parameters -> Modify FilePath)
+    -- ^ Getter for path modification function that takes base directory and
+    -- returns path to one of its subdirectories.
+    -> Parameters
     -> IO FilePath
     -- ^ Absolute file path.
-stdGetDir envVarName subdir = do
-    maybeDir <- getEnv' envVarName
+stdGetDir getEnvVarName getModifyFilePath params = do
+    maybeDir <- getEnv envVarName
     case maybeDir of
         Just dir -> return dir
-        Nothing  -> (</> subdir) <$> getBaseDir
+        Nothing  -> modifyFilePath <$> getBaseDir params
+  where
+    envVarName     = getEnvVarName params
+    modifyFilePath = getModifyFilePath params
 
-getLibDir :: IO FilePath
-getLibDir = stdGetDir "data_files_libdir" "lib"
+getLibDir :: Parameters -> IO FilePath
+getLibDir = stdGetDir libDirEnvVar baseDirToLibDir
 
-getDataDir :: IO FilePath
-getDataDir = stdGetDir "data_files_datadir" "share"
+getDataDir :: Parameters -> IO FilePath
+getDataDir = stdGetDir libDirEnvVar baseDirToLibDir
 
-getSysconfigDir :: IO FilePath
-getSysconfigDir = stdGetDir "data_files_sysconfdir" "etc"
+getSysconfigDir :: Parameters -> IO FilePath
+getSysconfigDir = stdGetDir sysconfigDirEnvVar baseDirToSysconfigDir
 
-getLibexecDir :: IO FilePath
-getLibexecDir = stdGetDir "data_files_libexecdir" "libexec"
+getLibexecDir :: Parameters -> IO FilePath
+getLibexecDir = stdGetDir libexecDirEnvVar baseDirToLibexecDir
 
-getDataFileName :: FilePath -> IO FilePath
-getDataFileName relativeFilePath = (</> relativeFilePath) <$> getDataDir
+getDataFileName :: Parameters -> FilePath -> IO FilePath
+getDataFileName params relativeFilePath = (</> relativeFilePath) <$> getDataDir params
 
 -- | Utility function that returns 'Nothing' if environment variable is not
--- defined. Otherwise it behaves as 'getEnv'.
-getEnv'
+-- defined. Otherwise it behaves as 'Environment.getEnv' form
+-- "System.Environment" module.
+getEnv
     :: String
-    -- ^ Environment variable name.
+    -- ^ Environment variable name, if its empty 'Nothing' is returned.
     -> IO (Maybe String)
-getEnv' var = (Just <$> getEnv var)
+getEnv ""  = return Nothing
+getEnv var = (Just <$> Environment.getEnv var)
     `catch` forget ioException (return Nothing)
   where
     ioException = Proxy :: Proxy IOException
@@ -123,9 +149,9 @@ getEnv' var = (Just <$> getEnv var)
     forget :: Proxy b -> a -> b -> a
     forget Proxy = const
 
--- $intro
+-- $directoryLayout
 --
--- Expected directory layout:
+-- Default directory layout:
 --
 -- > ${base_dir}
 -- > |-- bin/${executable}
@@ -133,3 +159,11 @@ getEnv' var = (Just <$> getEnv var)
 -- > |-- lib/
 -- > |-- libexec/
 -- > `-- share/
+--
+-- Value of @${base_dir}@ can be adjusted either by environment variable
+-- defined in 'baseDirEnvVar' or by changing function 'executableDirToBinDir'
+-- that derives its value from directory path where currently running
+-- executable was found.
+--
+-- Other directories have similar ways for overriding their value and deriving
+-- it from @${base_dir}@. See 'Parameters' data type for details.
---
+++ src/System/Environment/RelativeDataFiles/Parameters.hs
@@ -0,0 +1,89 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+-- |
+-- Module:       $HEADER$
+-- Description:  TODO
+-- Copyright:    (c) 2015, Peter Trško
+-- License:      BSD3
+--
+-- Maintainer:   peter.trsko@gmail.com
+-- Stability:    experimental
+-- Portability:  DeriveDataTypeable, DeriveGeneric, NoImplicitPrelude
+--
+-- TODO
+module System.Environment.RelativeDataFiles.Parameters
+    ( Parameters(..)
+    , Modify
+    )
+  where
+
+import Data.Function ((.))
+import Data.List ((++))
+import Data.String (String)
+import Data.Typeable (Typeable)
+import GHC.Generics (Generic)
+import System.IO (FilePath)
+
+import System.FilePath ((</>), dropFileName, dropTrailingPathSeparator)
+
+import Data.Default.Class (Default(def))
+
+
+type Modify a = a -> a
+
+data Parameters = Parameters
+    { baseDirEnvVar          :: String
+    , binDirEnvVar           :: String
+    , dataDirEnvVar          :: String
+    , libDirEnvVar           :: String
+    , libexecDirEnvVar       :: String
+    , sysconfigDirEnvVar     :: String
+    , baseDirToBinDir        :: Modify FilePath
+    , baseDirToDataDir       :: Modify FilePath
+    , baseDirToLibDir        :: Modify FilePath
+    , baseDirToLibexecDir    :: Modify FilePath
+    , baseDirToSysconfigDir  :: Modify FilePath
+    , executableDirToBaseDir :: Modify FilePath
+    , executableDirToBinDir  :: Modify FilePath
+    }
+  deriving (Generic, Typeable)
+
+-- | Environment variables are defined the same way as Cabal does. Definition
+-- looks like:
+--
+-- @
+-- 'def' = 'Parameters'
+--     { 'baseDirEnvVar'          = \"data_files_basedir\"
+--     , 'binDirEnvVar'           = \"data_files_bindir\"
+--     , 'dataDirEnvVar'          = \"data_files_datadir\"
+--     , 'libDirEnvVar'           = \"data_files_libdir\"
+--     , 'libexecDirEnvVar'       = \"data_files_libexecdir\"
+--     , 'sysconfigDirEnvVar'     = \"data_files_sysconfigdir\"
+--     , 'baseDirToBinDir'        = ('</>' \"bin\")
+--     , 'baseDirToDataDir'       = ('</>' \"share\")
+--     , 'baseDirToLibDir'        = ('</>' \"lib")
+--     , 'baseDirToLibexecDir'    = ('</>' \"libexec\")
+--     , 'baseDirToSysconfigDir'  = ('</>' \"etc\")
+--     , 'executableDirToBaseDir' = 'dropTrailingPathSeparator' . 'dropFileName'
+--     , 'executableDirToBinDir'  = 'dropTrailingPathSeparator'
+--     }
+-- @
+instance Default Parameters where
+    def = Parameters
+        { baseDirEnvVar          = mkEnvVar "base"
+        , binDirEnvVar           = mkEnvVar "bin"
+        , dataDirEnvVar          = mkEnvVar "data"
+        , libDirEnvVar           = mkEnvVar "lib"
+        , libexecDirEnvVar       = mkEnvVar "libexec"
+        , sysconfigDirEnvVar     = mkEnvVar "sysconfig"
+        , baseDirToBinDir        = (</> "bin")
+        , baseDirToDataDir       = (</> "share")
+        , baseDirToLibDir        = (</> "lib")
+        , baseDirToLibexecDir    = (</> "libexec")
+        , baseDirToSysconfigDir  = (</> "etc")
+        , executableDirToBaseDir = dropTrailingPathSeparator . dropFileName
+        , executableDirToBinDir  = dropTrailingPathSeparator
+        }
+      where
+        mkEnvVar dirId = "data_files_" ++ dirId ++ "dir"
