34c7ac4decc84551431474be36d38a295c380c4e
Peter Trško <peter.trsko@gmail.com>
2015-01-25 20:54:49 +0100

    Updated and extended documentation

--- src/System/Environment/InstallPaths/Parameters.hs
+++ src/System/Environment/InstallPaths/Parameters.hs
@@ -3,7 +3,8 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 -- |
 -- Module:       $HEADER$
--- Description:  TODO
+-- Description:  Parameters of functions in System.Environment.InstallPaths
+--               module.
 -- Copyright:    (c) 2015, Peter Trško
 -- License:      BSD3
 --
@@ -11,7 +12,7 @@
 -- Stability:    experimental
 -- Portability:  DeriveDataTypeable, DeriveGeneric, NoImplicitPrelude
 --
--- TODO
+-- Parameters of functions in "System.Environment.InstallPaths" module.
 module System.Environment.InstallPaths.Parameters
     (
     -- * Symbolic Names For Directories
@@ -63,6 +64,7 @@ type Modify a = a -> a
 
 -- {{{ Symbolic Names For Directories -----------------------------------------
 
+-- | Enum of directly supported directories.
 data Directory
     = BaseDir
     | BinDir
@@ -72,6 +74,19 @@ data Directory
     | SysconfigDir
   deriving (Bounded, Data, Enum, Eq, Generic, Ord, Show, Typeable)
 
+-- | Return default environment variable name for a specific directory. If that
+-- variable is found, when trying to get file path of that specific directory,
+-- its value is used instead of deriving it from executable path.
+--
+-- @
+-- 'defaultEnvironmentVariable' dir = case dir of
+--     'BaseDir'      -> \"data_files_basedir\"
+--     'BinDir'       -> \"data_files_bindir\"
+--     'DataDir'      -> \"data_files_datadir\"
+--     'LibDir'       -> \"data_files_libdir\"
+--     'LibexecDir'   -> \"data_files_libexecdir\"
+--     'SysconfigDir' -> \"data_files_sysconfigdir\"
+-- @
 defaultEnvironmentVariable :: Directory -> String
 defaultEnvironmentVariable dir = case dir of
     BaseDir      -> mkEnvVar "base"
@@ -83,10 +98,26 @@ defaultEnvironmentVariable dir = case dir of
   where
     mkEnvVar dirId = "data_files_" ++ dirId ++ "dir"
 
--- | @'noEnvironmentVariable' = 'const' \"\"@
+-- | If this function is used as a value of 'environmentVariable', then
+-- functions in "System.Environment.InstallPaths" don't check environment
+-- variables.
+--
+-- @'noEnvironmentVariable' = 'const' \"\"@
 noEnvironmentVariable :: Directory -> String
 noEnvironmentVariable = const ""
 
+-- | Take base directory path and return path for a specified 'Directory'. For
+-- base directory this function behaves as identity.
+--
+-- @
+-- 'defaultBaseDirTo' dir = case dir of
+--     'BaseDir'      -> 'id'
+--     'BinDir'       -> ('</>' \"bin\")
+--     'DataDir'      -> ('</>' \"share\")
+--     'LibDir'       -> ('</>' \"lib\")
+--     'LibexecDir'   -> ('</>' \"libexec\")
+--     'SysconfigDir' -> ('</>' \"etc\")
+-- @
 defaultBaseDirTo :: Directory -> Modify FilePath
 defaultBaseDirTo dir = case dir of
     BaseDir      -> id
@@ -96,16 +127,29 @@ defaultBaseDirTo dir = case dir of
     LibexecDir   -> (</> "libexec")
     SysconfigDir -> (</> "etc")
 
+-- | Default function for fuguring out directory file path from executable
+-- path.
+--
+-- Normally this function is used only for 'BaseDir' and 'BinDir', but it
+-- defaults to using 'defaultBaseDirTo' for other values of 'Directory'.
+--
+-- @
+-- 'defaultExecutableDirTo' dir = case dir of
+--     'BaseDir' -> 'dropTrailingPathSeparator' . 'dropFileName'
+--     'BinDir'  -> 'dropTrailingPathSeparator'
+--     _       -> 'defaultBaseDirTo' dir . 'defaultExecutableDirTo' 'BaseDir'
+-- @
 defaultExecutableDirTo :: Directory -> Modify FilePath
 defaultExecutableDirTo dir = case dir of
-    BaseDir      -> dropTrailingPathSeparator . dropFileName
-    BinDir       -> dropTrailingPathSeparator
-    _            -> defaultBaseDirTo dir . defaultExecutableDirTo BaseDir
+    BaseDir -> dropTrailingPathSeparator . dropFileName
+    BinDir  -> dropTrailingPathSeparator
+    _       -> defaultBaseDirTo dir . defaultExecutableDirTo BaseDir
 
 -- }}} Symbolic Names For Directories -----------------------------------------
 
 -- {{{ Parameters -------------------------------------------------------------
 
+-- | Parameters of functions in "System.Environment.InstallPaths" module.
 data Parameters = Parameters
     { environmentVariable :: Directory -> String
     , baseDirTo           :: Directory -> Modify FilePath
